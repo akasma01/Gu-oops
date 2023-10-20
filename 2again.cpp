a9f45d402bec98a6e829b702ece82df5a9aef662
akasma01 <97661850+akasma01@users.noreply.github.com>
2023-10-20 11:02:16 +0530

    Create 2again.cpp

---
+++ 2again.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+class date
+{
+    int day,month,year;
+    public:
+    void inputDate();
+    void display();
+    void addDate(int n);
+};
+
+void date::inputDate()
+{
+    cout<<"Enter day\n";
+    cin>>day;
+    cout<<"Enter month\n";
+    cin>>month;
+    cout<<"Enter year\n";
+    cin>>year;
+}
+
+void date::display()
+{
+    cout<<day<<"-"<<month<<"-"<<year;
+}
+
+void date::addDate(int n)
+{
+    int m=0,d=0,y=0;
+    if(n>30)
+    {
+        while(n>30)
+        {
+            n=n-30;
+            m++;
+        }
+        while(m>12)
+        {
+            m-=12;
+            y++;
+        }
+    }
+    else
+    {
+    d=n;
+    }
+
+    day+=d;
+    month+=m;
+    year+=y;
+    
+    while(day>30)
+    {
+        day-=30;
+        month++;
+    }
+    while(month>12)
+    {
+        month-=12;
+        year++;
+    }
+    
+    cout<<"The new date is: \n";
+    display();
+}
+
+
+int main()
+{
+    date d;
+    int n;
+    d.inputDate();
+    cout<<"Enter the no. of days you want to add\n";
+    cin>>n;
+    d.addDate(30);
+    
+    
+    return 0;
+}
